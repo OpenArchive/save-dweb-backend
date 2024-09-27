dc2d6856577fec6d2ac0e2a386d8c2abc6b4c6d7
tripledoublev <vincentcharlebois@gmail.com>
2024-09-27 13:30:26 -0400

    Add constants for communication protocol flags

--- src/constants.rs
+++ src/constants.rs
@@ -7,3 +7,11 @@ pub const FAILED_TO_LOAD_KEYPAIR: &str = "Failed to load keypair";
 pub const KEYPAIR_NOT_FOUND: &str = "Keypair not found";
 pub const FAILED_TO_DESERIALIZE_KEYPAIR: &str = "Failed to deserialize keypair";
 pub const ROUTE_ID_DHT_KEY: u32 = 2;
+
+pub const NO: u8 = 0x00u8;
+pub const YES: u8 = 0x01u8;
+pub const HAS: u8 = 0x10u8;
+pub const ASK: u8 = 0x11u8;
+pub const DATA: u8 = 0x20u8;
+pub const DONE: u8 = 0x22u8;
+pub const ERR: u8 = 0xF0u8;
